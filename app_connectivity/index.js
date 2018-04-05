5886e303683ba75adab22e9a7d07659f1c995913
Marcos O'Mahony <marcos.omahony.alonso@alumnos.upm.es>
2018-04-05 09:27:14 +0200

    Ñeñe

---
+++ app_connectivity/index.js
@@ -0,0 +1,13 @@
+const WebSocketServer = require( 'ws' ).Server;
+
+var wss = new WebSocketServer({port: 8085});
+wss.on('connection', function(ws) {
+    ws.on('message', (message) => {
+        console.log(' received: %s ', message) ;
+        setTimeout(() => {
+            console.log(' resending: %s ', message) 
+            ws.send(`Hola ${message}`)
+        }, 500);
+    });
+    ws.send('something') ;
+});
